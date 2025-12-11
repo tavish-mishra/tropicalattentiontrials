8f16324781d0b93c3877a35df606b124fdc598e1
Tavish Mishra <tavish@Tavishs-MacBook-Pro.local>
2025-12-11 10:18:26 -0800

    fix

--- experiment.py
+++ experiment.py
@@ -116,7 +116,7 @@ class Experiment:
             ckpt_path = candidates[0]
 
             #LET US SEE WHAT HAPPENS HERE
-            ckpt_path = '15_exp/models/FloydWarshallDataset_tropical_0.0001_100_20251114_080805_relu_best.pth'
+            ckpt_path = '15_exp/models/FloydWarshallDataset_tropical_0.0001_20000_20251205_163813_relu_best.pth'
     
             # 4) Load and record which model we evaluated
             state_dict = torch.load(ckpt_path, map_location=self.device)
