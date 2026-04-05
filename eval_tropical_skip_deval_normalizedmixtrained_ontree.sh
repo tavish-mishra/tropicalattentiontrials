f3be47112e70bd8a14b4340b35baf6139daddaeb
Tavish Mishra <tavish@Tavishs-MacBook-Pro.local>
2026-04-04 21:05:08 -0700

    ok wrote validation scripts rq

---
+++ eval_tropical_skip_deval_normalizedmixtrained_ontree.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -A m4790
+#SBATCH -C gpu
+#SBATCH -q regular
+#SBATCH -t 04:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=10
+#SBATCH -J exp_15
+#SBATCH -o logs/%x-%j.out
+#SBATCH -e logs/%x-%j.err
+
+mkdir -p logs
+
+module load cudatoolkit
+module load pytorch
+module load conda
+
+conda activate tropical_attention
+
+export OMP_NUM_THREADS=10
+
+echo "CUDA available? $(python -c 'import torch; print(torch.cuda.is_available())')"
+echo "GPU device: $(python -c 'import torch; print(torch.cuda.get_device_name(0))')"
+echo "CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+echo "SLURM_JOB_GPUS=$SLURM_JOB_GPUS"
+
+python experiment.py --job_file jobs_to_do_evaluate --job_id 37 --tag 15_exp --skip --checkpoint 15_exp/models/FloydWarshallDataset_tropical_0.0001_20000_20260403_100456_relu_best.pth --graph_type tree
+
